28cb2744915d6c325c701065fb00e68f3bdd9867
unknown <tatpowdev@gmail.com>
2026-08-25 15:13:01 +0300

    Переезд на Godot v.4.7.2 (.NET)

--- PPSS Source Game OLD/Assets/NPC Generator/DataFormat.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class DataFormat
-{
-    public int day = 1;
-    public int month = 1;
-    public int year = 2000;
-}
--- PPSS Source Game OLD/Assets/NPC Generator/Documents/BordingPass.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/Document/Bording Pass")]
-public class BordingPass : BaseDocument
-{
-    public int Number;
-
-    public override DocumentType Type => DocumentType.BordingPass;
-}
